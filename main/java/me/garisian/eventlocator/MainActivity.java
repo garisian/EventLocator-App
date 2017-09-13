171085cb4e71cecdb9896f34cf812ade180e56be
garisian <garisian22@gmail.com>
2017-09-13 01:25:24 -0400

    making sure everything is clean

--- main/java/me/garisian/eventlocator/MainActivity.java
+++ main/java/me/garisian/eventlocator/MainActivity.java
@@ -115,10 +115,10 @@ public class MainActivity extends AppCompatActivity implements GoogleMap.OnMarke
     public void onMapReady(GoogleMap googleMap) {
         // Add a marker in Sydney, Australia,
         // and move the map's camera to the same location.
-        LatLng sydney = new LatLng(43.6730796,-79.46743909999999);
-        googleMap.addMarker(new MarkerOptions().position(sydney)
+        LatLng toronto = new LatLng(43.6730796,-79.46743909999999);
+        googleMap.addMarker(new MarkerOptions().position(toronto)
                 .title("Marker in Toronto"));
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(toronto));
         googleMap.animateCamera( CameraUpdateFactory.zoomTo( 17.0f ) );
         googleMap.setOnMarkerClickListener(this);
 
